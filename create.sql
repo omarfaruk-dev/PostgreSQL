b87894a82655de95d84f173822db8c0b5e850a81
omarfaruk-dev <omarfaruk.dev@gmail.com>
2025-11-24 21:14:44 +0600

    first commit

---
+++ create.sql
@@ -0,0 +1,9 @@
+-- Active: 1763995340694@@localhost@5432@alibaba
+-- current database
+-- create bd
+-- create user table
+-- insert data into table
+
+-- ? task - 1: 
+
+SELECT datname FROM pg_database;
